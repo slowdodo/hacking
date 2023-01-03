a2b70b90490247cec9e7d3d3d5f45ccd27d27615
slowdodo <96872399+slowdodo@users.noreply.github.com>
2023-01-04 03:58:15 +0900

    Update scan.ps1

--- window/scan.ps1
+++ window/scan.ps1
@@ -1,1 +1,1 @@
-1..255 | ForEach-Object { $response = [System.Net.NetworkInformation.Ping]::Ping("10.0.$_.1") if ($response.Status -eq "Success") { Write-Output "10.0.$_.1 is online" } }
+1..255 | ForEach-Object { Test-NetConnection -ComputerName "10.0.$_.1" -InformationLevel Quiet }
